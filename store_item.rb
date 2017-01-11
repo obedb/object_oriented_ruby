ec72831799fac2cbf6388f3ee6fe5be2f569b1be
Obed Bigirimana <obedbigirimana@gmail.com>
2017-01-11 14:32:47 +0200

    added store item file in the folder

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+car = {"color" => "blue", "price" => 20000, "brand" => "volvo"}
+computer = {"color" => "green", "price" => 3000, "brand" => "hp"}
+table = {"color" => "white", "price" => 200, "brand" => "new"}
+
+puts car["color"]
